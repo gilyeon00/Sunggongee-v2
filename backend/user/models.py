8e04746f987fa0bc85fb4a9cf9f326d10f6118e3
gilyeon00 <12venus08@naver.com>
2023-02-09 02:56:40 +0900

    feat: mysql db docker setting

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Equip(models.Model):
+    id = models.IntegerField(primary_key=True)
+    equip_name = models.CharField(max_length=100)
+    equip_code = models.CharField(max_length=10)
+    cost = models.IntegerField(default=0)

e04e24787bb573b0c2f8b58a423693c5cfad96e1
gilyeon00 <12venus08@naver.com>
2023-02-09 17:37:34 +0900

    feat: slider 구현

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -3,6 +3,7 @@ import SideMenu from '../component/SideMenu';
 import EquipList from '../component/EquipList';
 import { useDispatch, useSelector } from 'react-redux';
 import { equipListLayout } from '../store/equip/equipSlice';
+import HotEquipSlider from '../component/HotEquipSlider';
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -15,8 +16,8 @@ const Home = () => {
 
     return (
         <>
-            <SideMenu/>
                 <section className='main-content'>
+                    <HotEquipSlider/>
                     { listLayout && <EquipList display={listLayout} /> }
                 </section>
         </>

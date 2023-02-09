39ede5ab459d450c7819c6314ea83490e421cc1d
gilyeon00 <12venus08@naver.com>
2023-02-09 17:38:35 +0900

    feat: item별 detail 접근 및 화면구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,18 @@ import Home from './page/Home';
 import Header from './component/Header';
 import Login from './component/Login';
 import UserRedirect from './component/Login/UserRedirect';
-
+import SideMenu from './component/SideMenu';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header/>
+        <SideMenu/>
         <section>
           <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />  
+            <Route path="/login" element={<Login/>} />   
             <Route path="/equip/:id" element={<UserRedirect/>} />  
           </Routes>
         </section>

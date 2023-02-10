0f5a739e5e9f24e51b0e9f5ad646826efab272a1
gilyeon00 <12venus08@naver.com>
2023-02-10 10:39:39 +0900

    feat: 1000단위 금액 쉼표표시 추가

--- frontend/src/component/EquipItem/index.jsx
+++ frontend/src/component/EquipItem/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import './index.css'
+import { unitCheck } from '../../lib/unitCheck';
 
 const EquipItem = ({item}) => {
 
@@ -21,7 +22,7 @@ const EquipItem = ({item}) => {
                 </div>
                 <div className='description-box'>
                     <div className='equip-name'>{item.equip_title}</div>
-                    <div className='equip-price'>{item.price}</div>
+                    <div className='equip-price'>{unitCheck(item.price)}</div>
                 </div>
             {/* </div> */}
         </div>
